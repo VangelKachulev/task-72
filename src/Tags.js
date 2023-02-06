7c18ad0b584d5b8a69d714d8e3d3704ecd6d628e
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-06 21:37:09 +0200

    test

--- src/Tags.js
+++ src/Tags.js
@@ -6,10 +6,9 @@ export default function Tags(props) {
     // console.log(props.props);
 
     return (
-        <ul>{props.props.map(e => <li  className="tag">#{e}</li>)}</ul>
+        <ul>{props.props?.map(e => <li  className="tag">#{e}</li>)}</ul>
     )
-
-
-
-
 }
+
+
+
