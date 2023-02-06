69cd9630189170956f1ac8dc2a225a7555b50222
VangelKachulev <vangel.kachulev@abv.bg>
2023-02-06 22:22:32 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ let tags = ["boomdotdev", "task", "tags", "react"];
 
 function App() {
   return (
-    <div className="App">
+   
       <Tags tags={tags}></Tags>
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
-    </div>
+      
   );
 }
 
